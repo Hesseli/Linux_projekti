8608b3f9d5d5cdab2da1a410362d89cefa289ace
Jesse <jesse.aittala@hotmail.com>
2025-10-29 12:32:40 +0200

    simppeliä!

--- server/helper/ApiError.js
+++
@@ -1,9 +0,0 @@
-// Mahdollistaa virheilmoituksen ja status koodin välittämisen
-class ApiError extends Error {
-  constructor(message, statusCode) {
-    super(message)
-    this.statusCode = statusCode
-  }
-}
-
-export { ApiError }
--- server/helper/Test.js
+++
@@ -1,6 +0,0 @@
-import request from 'supertest'
-import app from '../index.js'
-import { expect } from 'chai'
-
-export const api = request(app)
-export { expect }
