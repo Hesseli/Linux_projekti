81dcc97d16bea2af7ff7b6c101237603ac02aa79
Jesse <jesse.aittala@hotmail.com>
2025-12-11 18:04:24 +0200

    API testiä muokattu

--- cicd/tests/test_api.py
+++ cicd/tests/test_api.py
@@ -1,5 +1,12 @@
 import requests
+import time
 
-def test_root():
-    r = requests.get("http://backend:5000/messages")
+def test_backend_messages():
+    # Odotetaan hetki, että backend ehtii käynnistyä GH-ympäristössä
+    time.sleep(2)
+
+    r = requests.get("http://localhost:5000/messages")
     assert r.status_code == 200
+    
+    data = r.json()
+    assert isinstance(data, list)
