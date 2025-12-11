1572d5f450afc64f527b9a02e5b47a55b2897411
Jesse <jesse.aittala@hotmail.com>
2025-12-11 18:09:27 +0200

    get osoitteen muutos

--- cicd/tests/test_api.py
+++ cicd/tests/test_api.py
@@ -1,12 +1,12 @@
 import requests
 import time
 
-def test_backend_messages():
-    # Odotetaan hetki, että backend ehtii käynnistyä GH-ympäristössä
+def test_root():
+    # Anna kontille hetki käynnistyä
     time.sleep(2)
 
     r = requests.get("http://localhost:5000/messages")
     assert r.status_code == 200
-    
+
     data = r.json()
     assert isinstance(data, list)
